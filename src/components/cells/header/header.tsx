b06e3ffe9d92f95c578588c4288d68a276e69be3
shuyanov <shuyanov@shuyanovs-MacBook-Pro.local>
2024-04-14 10:36:35 +0300

    feat(button): add manu and back button for header

--- src/components/cells/header/header.tsx
+++ src/components/cells/header/header.tsx
@@ -1,12 +1,64 @@
-import sun from "../../../assets/sun.svg";
+// import sun from "../../../assets/sun.svg";
+import Button from "@material-ui/core/Button";
 import styles from "./style.module.scss";
+import { useState } from "react";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import backIcon from "assets/linkIcon/back.svg";
 
 export const Header = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  var currentUrl = window.location.pathname;
+  console.log(currentUrl);
+
   return (
     <header className={styles.headerContainer}>
-      {/* <a href="#">My projects</a>
-      <a href="#">My experience</a> */}
-      <img src={sun} />
+      {currentUrl != "/my-web-cv" && (
+        <Button aria-controls="simple-menu" aria-haspopup="true">
+          <a href="/my-web-cv">
+            <img src={backIcon} className={styles.backButton} />
+          </a>
+        </Button>
+      )}
+      <Button
+        className={styles.menuButton}
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        My experience
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>
+          <a href="/digitable" className={styles.atribute}>
+            Digitable
+          </a>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <a href="/garpix" className={styles.atribute}>
+            Garpix
+          </a>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <a href="my-web-cv/cnet" className={styles.atribute}>
+            C.NET
+          </a>
+        </MenuItem>
+      </Menu>
     </header>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,23 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Cnet } from "./components/view/Cnet.tsx";
+import { Header } from "./components/cells/header/header.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/my-web-cv",
     element: <App />,
   },
+  {
+    path: "my-web-cv/cnet",
+    element: <Cnet />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Header />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
