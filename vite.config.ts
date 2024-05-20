1b801d29e290ba0286e162adf86ec09498703a66
Sweet62 <maks_shuyanov@mail.ru>
2024-05-20 23:29:08 +0300

    ci(fix): update path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/my-web-cv.github.io/",
+  base: "/my-web-cv/",
   resolve: {
     alias: {
       src: "/src",
